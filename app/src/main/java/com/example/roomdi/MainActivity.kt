9b4cbab0703f8ece123880e74ffbc84f19003df8
L <leonardo.alvirubro@hotmail.com>
2023-03-18 14:20:49 -0300

    refactor: some changes

--- app/src/main/java/com/example/roomdi/MainActivity.kt
+++ app/src/main/java/com/example/roomdi/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.roomdi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdi.adapter.CarListAdapter
+import com.example.roomdi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var adapter: CarListAdapter
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        this.recyclerView = this.binding.recyclerView
+        this.adapter = CarListAdapter()
+        this.recyclerView.adapter = this.adapter
+        this.recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
